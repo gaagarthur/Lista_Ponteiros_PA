6aaade0bb56561da0c19c3207f48c93e7413951c
Arthur Gaag <arthurgaagduarte@gmail.com>
2024-04-03 23:53:54 -0300

    updated Q25.c

--- Q25/Q25.c
+++ Q25/Q25.c
@@ -3,23 +3,22 @@
 
 int main(void){
 
-int **mat;
+int ***mat;
 int height, length, width;
 
 printf(" entre com altura,\n largura e profundidade ma matriz:");
 scanf("%d %d %d", &height, &length, &width);
 
 
-mat = malloc(width*sizeof(int**));//alocar matriz auxiliar 1
+mat = (int***)malloc(width*sizeof(int**));//alocar matriz auxiliar 1
 
-mat[0] = malloc(width*height*sizeof(int*));//alocar matriz auxiliar 2
+mat[0] = (int**)malloc(width*height*sizeof(int*));//alocar matriz auxiliar 2
   for(int i=1;i<height; i++){
     mat[i] = mat[i-1]+height;
   }
-mat[0][0] = malloc(width*height*length*sizeof(int));
+mat[0][0] = (int*)malloc(width*height*length*sizeof(int));
   for(int i=1; i<(width*height); i++){
-      for(int j=1; j<)
-
+     
 
   }
 
